169669a7811e9f68d419f8201f31034a96f55a12
saeedeh <ss3767@cornell.edu>
2018-08-16 12:39:07 -0400

    edit config

--- js/gameJS_withCreateJS.js
+++ js/gameJS_withCreateJS.js
@@ -263,4 +263,30 @@ function onRestart(){
 
 startGame()
 
-////////////
+//////////// file
+document.addEventListener('deviceready', onDeviceReady, false);
+function onDeviceReady() {
+    function writeToFile(fileName, data) {
+        data = JSON.stringify(data, null, '\t');
+        window.resolveLocalFileSystemURL(cordova.file.dataDirectory, function (directoryEntry) {
+            directoryEntry.getFile(fileName, { create: true }, function (fileEntry) {
+                fileEntry.createWriter(function (fileWriter) {
+                    fileWriter.onwriteend = function (e) {
+                        // for real-world usage, you might consider passing a success callback
+                        console.log('Write of file "' + fileName + '"" completed.');
+                    };
+
+                    fileWriter.onerror = function (e) {
+                        // you could hook this up with our global error handler, or pass in an error callback
+                        console.log('Write failed: ' + e.toString());
+                    };
+
+                    var blob = new Blob([data], { type: 'text/plain' });
+                    fileWriter.write(blob);
+                }, errorHandler.bind(null, fileName));
+            }, errorHandler.bind(null, fileName));
+        }, errorHandler.bind(null, fileName));
+    }
+
+    writeToFile('example.json', { foo: 'bar' });
+}
